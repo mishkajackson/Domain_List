663a391fc847d6ca5c6788a342b152df6a459e6c
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2024-11-09 23:19:12 +0300

    Update generate_configs.py

--- generate_configs.py
+++ generate_configs.py
@@ -39,7 +39,7 @@ def generate_shadowrocket_conf(domain_suffix, domain_keyword):
         f.write("bypass-tun = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, "
                 "192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24, "
                 "224.0.0.0/4, 255.255.255.255/32\n")
-        f.write("dns-server = https://dns.adguard-dns.com/dns-query, 8.8.8.8, 8.8.4.4\n")
+        f.write("dns-server = 1.1.1.1, 1.0.0.1, 8.8.8.8, 8.8.4.4\n")
         f.write("fallback-dns-server = system\n")
         f.write("update-url = https://raw.githubusercontent.com/mishkajackson/Domain_List/refs/heads/main/Shadowrocket.conf\n\n")
         f.write("[Rule]\n")
